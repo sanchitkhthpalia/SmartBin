668613fb3b277ac24cb0235991707ddb6b92c15e
Sanchit Kathpalia <114855470+sanchitkhthpalia@users.noreply.github.com>
2025-06-22 22:09:59 +0530

    Update Hero.jsx

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage } from '@react-three/drei';
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls, Stage } from '@react-three/drei';
 import { Link } from 'react-router-dom';
 
 function Dustbin3D() {
